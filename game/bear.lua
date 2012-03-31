24580b0cadfcb109d8297b784bc79938e957df0f
Paul Du Bois <paul.dubois@gmail.com>
2012-03-31 16:23:00 -0700

    Tunable damping and force

--- game/bear.lua
+++ game/bear.lua
@@ -1,6 +1,7 @@
 -- I AM A FRIENDLY BEAR
 
-local FORCE_SCALE = 1
+local BEAR_FORCE = 20
+local BEAR_DAMPING = 1.7
 
 local Ob = {}
 Ob.move_actions = {}
@@ -11,18 +12,48 @@ function Ob:init()
     body:setTransform ( 10, 10 )
     body:setFixedRotation ( true )
     body:setMassData ( 1 )
+    body:setLinearDamping( BEAR_DAMPING )
     self.body = body
 
 	g_input.keymap.i = self:make_mover(0,1)
 	g_input.keymap.j = self:make_mover(-1,0)
 	g_input.keymap.k = self:make_mover(0,-1)
 	g_input.keymap.l = self:make_mover(1,0)
+
+    -- debug keybindings for tuning
+    g_input.keymap.t = function(k,d)
+        if d then
+            BEAR_FORCE = BEAR_FORCE * 1.05
+            print('force is now',BEAR_FORCE)
+        end
+    end
+    g_input.keymap.g = function(k,d)
+        if d then
+            BEAR_FORCE = BEAR_FORCE / 1.05
+            print('force is now',BEAR_FORCE)
+        end
+    end
+
+    g_input.keymap.y = function(k,d)
+        if d then
+            BEAR_DAMPING = BEAR_DAMPING * 1.05
+            self.body:setLinearDamping(BEAR_DAMPING)
+            print('damp is now',BEAR_DAMPING)
+        end
+    end
+    g_input.keymap.h = function(k,d)
+        if d then
+            BEAR_DAMPING = BEAR_DAMPING / 1.05
+            self.body:setLinearDamping(BEAR_DAMPING)
+            print('damp is now',BEAR_DAMPING)
+        end
+    end
 end
 
 function Ob:make_mover(dx,dy)
 	local function move_infinitely()
 		while true do
-			self.body:applyForce(dx * FORCE_SCALE, dy * FORCE_SCALE)
+			self.body:applyForce(dx * BEAR_FORCE, dy * BEAR_FORCE)
 			coroutine.yield()
 		end
 	end
